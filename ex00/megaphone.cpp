bf32fddb78cdafa89f7128992bf6841f99e22e7b
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-11-15 23:08:42 +0100

    first exercise done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	if (argc == 1)
+	{
+		std::cout << " LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string current = argv[i];
+			for (size_t j = 0; j < current.length(); j++)
+			{
+				std::cout << (char)std::toupper(current[j]);
+			}
+			if (i < argc - 1)
+			{
+				std::cout << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
