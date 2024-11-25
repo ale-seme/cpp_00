2e18056e632f8f8e0bb046d48d1481d98ba95786
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-11-25 14:23:31 +0100

    fully implemented the add method

--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -1,22 +1,23 @@
 #include "Contact.hpp"
+#include "RequestFunctions.hpp"
  
 Contact::Contact(){};
 
-void	Contact::setFirstName(const std::string &newName){
-	_firstName = newName;
-}
-void	Contact::setLastName(const std::string &newLast){
-	_lastName = newLast;
-}
-void	Contact::setNickName(const std::string &newNick){
-	_nickName = newNick;
-}
-void	Contact::setPhoneNumber(const std::string &newPhoneNbr){
-	_phoneNumber = newPhoneNbr;
-}
-void	Contact::setDarkestSecret(const std::string &newSecret){
-	_darkestSecret = newSecret;
-}
+// void	Contact::setFirstName(const std::string &newName){
+// 	_firstName = newName;
+// }
+// void	Contact::setLastName(const std::string &newLast){
+// 	_lastName = newLast;
+// }
+// void	Contact::setNickName(const std::string &newNick){
+// 	_nickName = newNick;
+// }
+// void	Contact::setPhoneNumber(const std::string &newPhoneNbr){
+// 	_phoneNumber = newPhoneNbr;
+// }
+// void	Contact::setDarkestSecret(const std::string &newSecret){
+// 	_darkestSecret = newSecret;
+// }
 
 std::string Contact::getFirstName() const{
 	return _firstName;
@@ -36,4 +37,13 @@ std::string Contact::getPhoneNumber() const{
 
 std::string Contact::getDarkestSecret() const{
 	return _darkestSecret;
+}
+
+void	Contact::inputContactDetails(){
+
+	_firstName = get_input("First Name: ");
+	_lastName = get_input("Last Name: ");
+	_nickName = get_input("Nick Name: ");
+	_phoneNumber = get_input ("Phone Number: ");
+	_darkestSecret = get_input("Darkest Secret: ");
 }
--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -4,26 +4,12 @@ PhoneBook::PhoneBook() : index(0) {};
 
 void	PhoneBook::add(void){
 
-	std::string input;
 	Contact NewContact;
-
-
+	NewContact.inputContactDetails();
+	contact_array[index] = NewContact;
+	index = (index + 1) % 8;
 	// input = get_input("Please insert the first name of your contact");
-	input = get_input("First Name: ");
 
-	PhoneBook::contact_array[0].setFirstName(input);
-
-	input = get_input("Last Name: ");
-	PhoneBook::contact_array[0].setLastName(input);
-
-	input = get_input("Nick Name: ");
-	PhoneBook::contact_array[0].setNickName(input);
-
-	input = get_input ("Phone Number: ");
-	PhoneBook::contact_array[0].setPhoneNumber(input);
-
-	input = get_input("Darkest Secret: ");
-	PhoneBook::contact_array[0].setDarkestSecret(input);
 }
 
 void	PhoneBook::search(){
@@ -32,11 +18,14 @@ void	PhoneBook::search(){
 
 void	PhoneBook::printContacts()
 {
-	std::cout << PhoneBook::contact_array[0].getFirstName() << std::endl;
-	std::cout << PhoneBook::contact_array[0].getLastName() << std::endl;
-	std::cout << PhoneBook::contact_array[0].getNickName() << std::endl;
-	std::cout << PhoneBook::contact_array[0].getPhoneNumber() << std::endl;
-	std::cout << PhoneBook::contact_array[0].getDarkestSecret() << std::endl;
+	for (auto& contact: contact_array)
+	{
+		std::cout << contact.getFirstName() << std::endl;
+		std::cout << contact.getLastName() << std::endl;
+		std::cout << contact.getNickName() << std::endl;
+		std::cout << contact.getPhoneNumber() << std::endl;
+		std::cout << contact.getDarkestSecret() << std::endl;
+	}
 }
 void	PhoneBook::exit(){
 	//exit();
