d315f48c9de57662a21a1fbaaa7b1274c0eda157
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-11-27 16:59:25 +0100

    started to implement ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+#include "Account.hpp"
+
+int Account::getNbAccounts( void ){
+	return	_nbAccounts;
+}
+
+int Account::getTotalAmount( void ){
+	return _totalAmount;
+}
+
+int Account::getNbDeposits( void ){
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals( void ){
+	return _totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos( void ){
+	
+}
