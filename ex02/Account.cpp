d6fc7d216886398e213ac784863d0ab406d79a64
Alessandro Semeraro <alexei.semeraro@gmail.com>
2024-11-29 14:04:56 +0100

    implemented other methods missing the display status method

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -11,26 +11,19 @@ int Account::_totalNbWithdrawals = 0;
 Account::Account( int initial_deposit ) : _amount(initial_deposit){
 	_nbAccounts += 1;
 	_totalAmount += initial_deposit;
-
-	_accountIndex = _nbAccounts;
+	_accountIndex = _nbAccounts - 1;
 	_amount = initial_deposit;
 	_nbDeposits = 0;
 	_nbWithdrawals = 0;
-	//function to display the timestamp;
-	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
-
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount 
+	<< ";created" << std::endl;
 }
 
-void	Account::_displayTimestamp(){
-	auto now = std::time(0);
-	auto tm = std::localtime(&now);
-
-	std::cout << "[" << 1900 + tm->tm_year
-		<< std::setw(2) << std::setfill('0') <<  1 + tm->tm_mon
-		<< std::setw(2) << std::setfill('0') << tm->tm_mday << "_"
-		<< std::setw(2) << std::setfill('0') << tm->tm_hour
-		<< std::setw(2) << std::setfill('0') << tm->tm_min
-		<< std::setw(2) << std::setfill('0') << tm->tm_sec << "] ";
+Account::~Account( void ){
+_displayTimestamp();
+std::cout << "index:" << _accountIndex << ";amount:" << _amount 
+<< ";closed;" <<std::endl;
 }
 
 int Account::getNbAccounts( void ){
@@ -49,13 +42,56 @@ int Account::getNbWithdrawals( void ){
 	return _totalNbWithdrawals;
 }
 
+void Account::makeDeposit( int deposit ){
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+	<< ";p_amount:" << _amount << ";deposit:" << deposit;
+	_amount += deposit;
+	_totalNbDeposits += 1;
+	_nbDeposits += 1;
+	_totalAmount += deposit; //can the total amount be more than MAX INT
+	//do something before to check for overflow;
+	std::cout<<";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal( int withdrawal ){
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex <<";p_amount:" << _amount
+	<< ";withdrawal:";
+	if (_amount < withdrawal)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_nbWithdrawals += 1;
+	_totalNbWithdrawals += 1;
+	std::cout << withdrawal << ";amount" << _amount
+	<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (true);
+}
+int	Account::checkAmount( void ) const{
+	return (_amount);
+}
+
 void	Account::displayAccountsInfos( void ){
 	
-	std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits\
-	<< ";withdrawals:" << _totalNbWithdrawals << std::endl;
+	_displayTimestamp();
 	//OR ALSO GOOD MAYBE
-	std::cout << "accounts:" << getNbAccounts() << ";";
-	std::cout << "total:" << getTotalAmount() << ";";
-	std::cout << "deposits:" << getNbDeposits() << ";";
-	std::cout << "withdrawals" << getNbWithdrawals() << std::endl;
+	std::cout << "accounts:" << getNbAccounts() << ";"
+	<< "total:" << getTotalAmount() << ";"
+	<< "deposits:" << getNbDeposits() << ";"
+	<< "withdrawals" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::_displayTimestamp(){
+	auto now = std::time(0);
+	auto tm = std::localtime(&now);
+	std::cout << "[" << 1900 + tm->tm_year
+		<< std::setw(2) << std::setfill('0') <<  1 + tm->tm_mon
+		<< std::setw(2) << std::setfill('0') << tm->tm_mday << "_"
+		<< std::setw(2) << std::setfill('0') << tm->tm_hour
+		<< std::setw(2) << std::setfill('0') << tm->tm_min
+		<< std::setw(2) << std::setfill('0') << tm->tm_sec << "] ";
 }
